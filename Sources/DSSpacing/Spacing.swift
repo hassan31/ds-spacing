0c4283b5d24790083b3e2f90379b8a3f48150ae9
Muhammad Hassan <>
2025-04-17 09:45:24 -0400

    added common spacings

---
+++ Sources/DSSpacing/Spacing.swift
@@ -0,0 +1,32 @@
+//
+//  Spacing.swift
+//  DSSpacing
+//
+//  Created by Muhammad Hassan on 2025-04-17.
+//
+
+import Foundation
+
+public struct Spacing {
+    public static let dp0: CGFloat = 0.0
+    public static let dp1: CGFloat = 1.0
+    public static let dp2: CGFloat = 2.0
+    public static let dp4: CGFloat = 4.0
+    public static let dp6: CGFloat = 6.0
+    public static let dp8: CGFloat = 8.0
+    public static let dp12: CGFloat = 12.0
+    public static let dp16: CGFloat = 16.0
+    public static let dp20: CGFloat = 20.0
+    public static let dp24: CGFloat = 24.0
+    public static let dp28: CGFloat = 28.0
+    public static let dp32: CGFloat = 32.0
+    public static let dp36: CGFloat = 36.0
+    public static let dp40: CGFloat = 40.0
+    public static let dp44: CGFloat = 44.0
+    public static let dp48: CGFloat = 48.0
+    public static let dp52: CGFloat = 52.0
+    public static let dp56: CGFloat = 56.0
+    public static let dp64: CGFloat = 64.0
+    public static let dp72: CGFloat = 72.0
+    public static let dp80: CGFloat = 80.0
+}
